37c98adad9f590032acc58b11651e6050ba14940
safartechlab <safartechlab@gmail.com>
2025-12-03 14:49:21 +0530

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 const fs = require("fs");
 const connectDB = require("./Utilities/connectdb");
